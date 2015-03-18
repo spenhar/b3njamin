131a58e8f04c52179f331f744f8b3690513a53aa
Spencer Harrison <spencerjharrison@gmail.com>
2015-03-18 02:02:44 -0400

    updated links and speed of swipe

--- js/script.js
+++ js/script.js
@@ -82,72 +82,73 @@ $(document).click(function() {
 
 var elem = document.getElementById('slider');
 window.mySwipe = Swipe(elem, {
-  startSlide: 3,
+  startSlide: Math.random()*14,
+  speed: 1400,
   auto: 4000,
   continuous: true,
   // disableScroll: true,
-  // stopPropagation: true,
+  stopPropagation: true,
   // callback: function(index, element) {},
   // transitionEnd: function(index, element) {}
 });
 
 
   // Smooth Touch Scrolljacking
-  // var  containerTop =     0, // keeps track of amount scrolled without actually scrolling
-  // moved =                 0, // amount your finger moved during touchmove
-  // touchStartY =           0,
-  // isTouchDevice =         false, // set to true on touchstart
-  // touchInterval;
+  var  containerTop =     0, // keeps track of amount scrolled without actually scrolling
+  moved =                 0, // amount your finger moved during touchmove
+  touchStartY =           0,
+  isTouchDevice =         false, // set to true on touchstart
+  touchInterval;
 
-  // // Initialize
-  // pageResize();
-  // scrollHandler();
-  // // Page events
-  // WIN.on('resize',pageResize);
-  // WIN.on('scroll',scrollHandler);
-  // // Touch events
-  // WIN.on('touchstart',function(e){
-  //   isTouchDevice = true;
-  //   touchInterval = setInterval(redraw, 10);
-  //   moved = 0;
-  //   var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-  //   touchStartY = touch.pageY;
-  // })
-  // WIN.on('touchmove',function(e){
-  //   e.preventDefault();
-  //   var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-  //   moved = touch.pageY-touchStartY;
-  //   containerTop -= moved;
-  //   touchStartY = touch.pageY;
-  // })
-  // WIN.on('touchend',function(e){
-  //   moved*=.95;
-  //   containerTop -= moved;
-  //   if(Math.abs(moved) < .2){
-  //     clearInterval(touchInterval);
-  //   }
-  // })
-  // function pageResize (e) {
-  //   WIN_H = WIN.height();
-  //   WIN_W = WIN.width();
+  // Initialize
+  pageResize();
+  scrollHandler();
+  // Page events
+  WIN.on('resize',pageResize);
+  WIN.on('scroll',scrollHandler);
+  // Touch events
+  WIN.on('touchstart',function(e){
+    isTouchDevice = true;
+    touchInterval = setInterval(redraw, 10);
+    moved = 0;
+    var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+    touchStartY = touch.pageY;
+  })
+  WIN.on('touchmove',function(e){
+    e.preventDefault();
+    var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+    moved = touch.pageY-touchStartY;
+    containerTop -= moved;
+    touchStartY = touch.pageY;
+  })
+  WIN.on('touchend',function(e){
+    moved*=.95;
+    containerTop -= moved;
+    if(Math.abs(moved) < .2){
+      clearInterval(touchInterval);
+    }
+  })
+  function pageResize (e) {
+    WIN_H = WIN.height();
+    WIN_W = WIN.width();
     
-  //   initialized = true;
-  // }
-  // function scrollHandler(){
-  //   if(isTouchDevice){
-  //     scrollTop = Math.max(0,containerTop);
-  //   }else{
-  //     scrollTop = WIN.scrollTop();
-  //   }
-  // }
+    initialized = true;
+  }
+  function scrollHandler(){
+    if(isTouchDevice){
+      scrollTop = Math.max(0,containerTop);
+    }else{
+      scrollTop = WIN.scrollTop();
+    }
+  }
 
-  // // Touch Scroll
-  // function redraw() {
-  //   if(isTouchDevice){
-  //     window.requestAnimationFrame(function() {
-  //       scrollHandler();
-  //     });
-  //   }
-  // }
+  // Touch Scroll
+  function redraw() {
+    if(isTouchDevice){
+      window.requestAnimationFrame(function() {
+        scrollHandler();
+      });
+    }
+  }
 
 })
